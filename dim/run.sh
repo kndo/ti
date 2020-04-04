a3161cb2e50d383b3425ad4c8fef012db7e7c6a7
Khanh Do <kdo@barracuda.com>
2020-04-04 04:07:10 -0700

    Remove unwanted run scripts

--- dim/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in $(ls *.gjf)
-    do
-    prefix=`basename $file .gjf`
-    g09 $file
-    mv FILE.43 $prefix.om
-    mv FILE.44 $prefix.fm
-    done
--- mon/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in $(ls *.gjf)
-    do
-    prefix=`basename $file .gjf`
-    g09 $file
-    mv fort.7 $prefix.mo
-    done
